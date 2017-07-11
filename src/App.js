58e02ee9f4b36cb79fac0fe9eea73c71fcc7386e
Bin Ury <bin.ury@tuta.io>
2017-07-11 16:32:47 -0500

    Add Normalize.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import { Provider } from 'react-redux';
 // import { createStore } from 'redux';
+import 'normalize.css';
 import './styles/App.css';
 import Navigation from './components/Navigation';
 
