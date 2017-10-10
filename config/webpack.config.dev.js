8ae29b22abb1a7b4c30935a162cf1d380e3dee00
Bin Ury <nesingwarysfinest@gmail.com>
2017-10-10 04:02:23 -0500

    Remove GoogleWebFont loader from development
    Sometimes this 404's which is super annoying because it throws an exception.  Lato can be installed in homebrew for local dev if  desired

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -10,7 +10,6 @@ const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeM
 const eslintFormatter = require('react-dev-utils/eslintFormatter');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
-const GoogleFontsPlugin = require("google-fonts-webpack-plugin");
 const getClientEnvironment = require('./env');
 const paths = require('./paths');
 
@@ -244,7 +243,6 @@ module.exports = {
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     // Favicon
     new FaviconsWebpackPlugin(`${paths.appSrc}/assets/favicon.png`),
-    new GoogleFontsPlugin({ fonts: [{ family: 'Lato' }] }),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
